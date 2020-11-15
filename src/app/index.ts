ede2083ff32e3968f5d2701d57757d4b9b7ce87b
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-11-14 21:57:18 -0300

    Setup morgan

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import helmet from 'helmet'
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(helmet())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => res.json({ foo: 'bar' }))
 
