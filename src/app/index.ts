45cc3e8eae8e47362384b3fe3027ce57a125ce81
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-11-19 21:47:14 -0300

    Add support for static files

--- src/app/index.ts
+++ src/app/index.ts
@@ -7,6 +7,8 @@ const app = express()
 app.use(helmet())
 app.use(morgan('dev'))
 
+app.use('/uploads', express.static('uploads'))
+
 app.get('/', (req, res) => res.json({ foo: 'bar' }))
 
 export default app
