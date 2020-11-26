ea872d8dd5b6a9d8cdafe769ac94dbf336482f41
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-11-25 21:34:02 -0300

    Defines use of trigger function update_timestamp()

--- src/database/migrations/20201118153513_create_users.ts
+++ src/database/migrations/20201118153513_create_users.ts
@@ -1,14 +1,22 @@
 import knex from 'knex'
 
 export async function up (knex: knex): Promise<void> {
-  return knex.schema.createTable('users', table => {
-    table.increments('id').primary()
-    table.string('name').notNullable().unique()
-    table.string('email').notNullable().unique()
-    table.string('password').notNullable()
-    table.string('reset_password_token').notNullable().defaultTo('')
-    table.timestamps(true, true)
-  })
+  return knex.schema
+    .createTable('users', table => {
+      table.increments('id').primary()
+      table.string('name').notNullable().unique()
+      table.string('email').notNullable().unique()
+      table.string('password').notNullable()
+      table.string('reset_password_token').notNullable().defaultTo('')
+      table.timestamps(true, true)
+    })
+    .raw(`
+      CREATE TRIGGER update_timestamp
+      BEFORE UPDATE
+      ON users
+      FOR EACH ROW
+      EXECUTE PROCEDURE update_timestamp()
+    `)
 }
 
 export async function down (knex: knex): Promise<void> {
--- src/database/migrations/20201118184308_create_points.ts
+++ src/database/migrations/20201118184308_create_points.ts
@@ -1,18 +1,26 @@
 import knex from 'knex'
 
 export async function up (knex: knex): Promise<void> {
-  return knex.schema.createTable('points', table => {
-    table.increments('id').primary()
-    table.string('name').notNullable().unique()
-    table.string('email').notNullable().unique()
-    table.integer('phone_number').notNullable().unique()
-    table.string('image_url').notNullable().unique()
-    table.integer('user_id').notNullable().references('id').inTable('users')
-    table.string('address').notNullable().unique()
-    table.string('city').notNullable()
-    table.string('state', 2).notNullable()
-    table.timestamps(true, true)
-  })
+  return knex.schema
+    .createTable('points', table => {
+      table.increments('id').primary()
+      table.string('name').notNullable().unique()
+      table.string('email').notNullable().unique()
+      table.integer('phone_number').notNullable().unique()
+      table.string('image_url').notNullable().unique()
+      table.integer('user_id').notNullable().references('id').inTable('users')
+      table.string('address').notNullable().unique()
+      table.string('city').notNullable()
+      table.string('state', 2).notNullable()
+      table.timestamps(true, true)
+    })
+    .raw(`
+      CREATE TRIGGER update_timestamp
+      BEFORE UPDATE
+      ON points
+      FOR EACH ROW
+      EXECUTE PROCEDURE update_timestamp()
+    `)
 }
 
 export async function down (knex: knex): Promise<void> {
