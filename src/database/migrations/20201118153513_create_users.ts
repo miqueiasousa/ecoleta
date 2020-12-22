896693378f0b0edda4bdbb5d240ece3f6c170b28
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-12-21 21:53:51 -0300

    Remove unique constraint on users table

--- src/database/migrations/20201118153513_create_users.ts
+++ src/database/migrations/20201118153513_create_users.ts
@@ -4,7 +4,7 @@ export async function up (knex: knex): Promise<void> {
   return knex.schema
     .createTable('users', table => {
       table.increments('id').primary()
-      table.string('name').notNullable().unique()
+      table.string('name').notNullable()
       table.string('email').notNullable().unique()
       table.string('password').notNullable()
       table.string('reset_password_token').notNullable().defaultTo('')
